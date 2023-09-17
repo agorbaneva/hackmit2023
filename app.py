e1e4782e841023ef7986cd0c7de603c4361a31f3
Alyssa Gorbaneva <alyssa.gorbaneva@duke.edu>
2023-09-17 10:35:28 -0400

    working forecasting

--- app.py
+++ app.py
@@ -5,32 +5,33 @@ import plotly.express as px
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.linear_model import LinearRegression
 from prophet import Prophet
+st.title("Breathe Easy: Shaping Tomorrow, One Model at a Time")
 
 # Load the excel file
 data = pd.read_excel('ByCountry.xlsx')
 
-# Get the unique values from the "C_group_IM24_sh" column
-c_group_values = data['C_group_IM24_sh'].unique()
+# Get the unique values from the "Name" column
+c_group_values = data['Name'].unique()
 
-# Create a select box widget with the unique "C_group_IM24_sh" values
-selected_c_group = st.selectbox('Select C Group:', c_group_values)
+# Create a select box widget with the unique "Name" values
+Name = st.selectbox('Select Country:', c_group_values)
 
-# Filter the data based on the selected "C_group_IM24_sh" value
-filtered_data = data[data['C_group_IM24_sh'] == selected_c_group]
+# Filter the data based on the selected "Name" value
+filtered_data = data[data['Name'] == Name]
 
 # Get the year columns
 year_columns = [col for col in data.columns if col.startswith('Y_')]
 
 # Create a dataframe for time series plot with years as one column and values as another column
-time_series_data = filtered_data.melt(id_vars=['C_group_IM24_sh'], value_vars=year_columns, var_name='Year', value_name='Value')
+time_series_data = filtered_data.melt(id_vars=['Name'], value_vars=year_columns, var_name='Year', value_name='Value')
+time_series_data['Year'] = time_series_data['Year'].str.extract('(\d+)').astype(int)
 
 # Plot the time series data using plotly
-fig = px.line(time_series_data, x='Year', y='Value', title=f'Time Series Data for {selected_c_group}')
+fig = px.line(time_series_data, x='Year', y='Value', title=f'Time Series Data for {Name}')
 st.plotly_chart(fig)
 
 
 # Prepare data for modeling
-time_series_data['Year'] = time_series_data['Year'].str.extract('(\d+)').astype(int)
 
 # ARIMA Model
 try:
